81de09d02f520779902e2801b9db8fca7af7b518
SharonLougheed <sharon.lougheed@gmail.com>
2019-02-17 06:11:54 -0600

    Basic Website Layout + Resume Page + Temporary Favicon
    Header script for all pages so no need to copy/paste. Embedded resume PDF on resume page. Animated favicon test.

---
+++ header.js
@@ -0,0 +1,29 @@
+document.write('\
+<!DOCTYPE html>\
+<html>\
+	<link rel="icon" \
+		  type="image/png" \
+		  href="favicon.png">\
+	<head>\
+		<title>Sharon Lougheed</title>\
+		<link rel="stylesheet" href="styles.css">\
+	</head>\
+	<body>\
+		<h1 ><a href="index.html" class="titleCard">Sharon Lougheed</a></h1>\
+		<div class="buttonPanel">\
+			<div class="lightningBorderButton">\
+				<a href="index.html" class="lightningInsideButton">Home</a>\
+			</div>\
+			<div class="lightningBorderButton">\
+				<a href="games.html" class="lightningInsideButton">Games</a>\
+			</div>\
+			<div class="lightningBorderButton">\
+				<a href="resume.html" class="lightningInsideButton">Resume</a>\
+			</div>\
+			<div class="lightningBorderButton">\
+				<a href="contact.html" class="lightningInsideButton">Contact</a>\
+			</div>\
+		</div>\
+	</body>\
+</html>\
+');
